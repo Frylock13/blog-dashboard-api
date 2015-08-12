f22f3607c56966752f064c4cc465074c57f41330
Vitaliy <croaton6@gmail.com>
2015-08-12 04:52:22 +0300

    added faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+6.times do
+  Dashboard::Post.create!(title: Faker::Lorem.sentence(3), 
+                          short: Faker::Lorem.sentence(10),
+                          desc:  Faker::Lorem.paragraph(5),
+                          topic: Faker::Lorem.word,
+                          created_at: Faker::Date.between(2.days.ago, Date.today),
+                          updated_at: Faker::Date.between(2.days.ago, Date.today)
+                          )
+end
