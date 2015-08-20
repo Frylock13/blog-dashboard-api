1177b58ce99730fcc9c9c81c088cc18b1327abca
Vitaliy <croaton6@gmail.com>
2015-08-20 18:33:03 +0300

    add index and user_id field

--- db/migrate/20150811045403_create_dashboard_posts.rb
+++ db/migrate/20150811045403_create_dashboard_posts.rb
@@ -6,8 +6,10 @@ class CreateDashboardPosts < ActiveRecord::Migration
       t.text :content
       t.string :tag
       t.boolean :published, default: true
-
+      t.integer :dashboard_posts
       t.timestamps null: false
     end
+
+    add_index :dashboard_postsm :user_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820142447) do
+ActiveRecord::Schema.define(version: 20150820151541) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,10 @@ ActiveRecord::Schema.define(version: 20150820142447) do
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
     t.boolean  "published",          default: true
+    t.integer  "user_id"
   end
+
+  add_index "dashboard_posts", ["user_id"], name: "index_dashboard_posts_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                        null: false
