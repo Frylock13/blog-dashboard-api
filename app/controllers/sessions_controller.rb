7ba6fbdd9adc3aafc99d561128cebba7d640a051
Vitaliy <croaton6@gmail.com>
2015-08-21 16:54:02 +0300

    add redirect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
+  def new
+    redirect_to dashboard_posts_path if current_user
+  end
+
   def create
     user = login(params[:email], params[:password], params[:remember_me])
     
     if user
       auto_login(user)
-      redirect_to dashboard_posts_path, :notice => "Logged in!"
+      redirect_to dashboard_posts_path
     else
       flash.now.alert = "Email or password was invalid"
       render :new
@@ -13,6 +17,6 @@ class SessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to root_path, :notice => "Logged out!"
+    redirect_to root_path
   end
 end
