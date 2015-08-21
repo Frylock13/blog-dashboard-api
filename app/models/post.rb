208ac54fdc9491c7546e0937a9bd6e2176ffb3a9
Vitaliy <croaton6@gmail.com>
2015-08-22 01:30:44 +0300

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 class Post < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :title, :short, :content, :tag, presence: true
 
   belongs_to :user
 
   enum status: %i(published unpublished archived)
 
   include PgSearch
-  pg_search_scope :search, against: [:title, :short, :content]
+  pg_search_scope :search, against: [:title, :short]
 
   scope :published, -> { where(status: 0) }
   scope :unpublished, -> { where(status: 1) }
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
 
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
+  validates :password, length: { in: 6..20 }
   validates_presence_of :email
   validates_uniqueness_of :email
 
