2081f5232516e9349d793b6bc49fb392905a7ba4
Vitaliy <croaton6@gmail.com>
2015-08-27 14:55:57 +0300

    remove friendly id for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,6 @@ class User < ActiveRecord::Base
 
   has_many :posts
 
-  extend FriendlyId
-  friendly_id :name, use: :slugged
-
   def generate_api_key
     update_attribute(:api_key, SecureRandom.urlsafe_base64(nil, false))
   end
