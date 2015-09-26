c64806f7ca587dcfb6e0212683ea16287ee46a90
Vitaliy <croaton6@gmail.com>
2015-09-26 11:26:31 +0300

    random status for seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,12 @@ User.create(email: "demo", password: "demo", password_confirmation: "demo")
 
 demo_user = User.find_by(email: "demo")
 
-6.times do
+13.times do
   Post.create!(title: Faker::Lorem.sentence(3),
                short: Faker::Lorem.sentence(10),
                content:  Faker::Lorem.paragraph(5),
                tags: Faker::Lorem.word,
-               status: 1,
+               status: rand(0..2),
                created_at: Faker::Date.between(2.days.ago, Date.today),
                updated_at: Faker::Date.between(2.days.ago, Date.today),
                user_id: demo_user.id
